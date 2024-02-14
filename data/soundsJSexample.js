f1b55660557c10c6730f6addbc002a9bdbf6743f
EmilieB10 <emilie.b10.kodehode@gmail.com>
2024-02-14 15:29:04 +0100

    UPDATED

--- data/soundsJSexample.js
+++ data/soundsJSexample.js
@@ -1,7 +1,11 @@
-
 export const sounds = [
-        { file: "pathTofile", key: "keyboardKeyTobePressed" },
-        { file: "pathTofile", key: "keyboardKeyTobePressed" },
-        { file: "pathTofile", key: "keyboardKeyTobePressed" },
-        { file: "pathTofile", key: "keyboardKeyTobePressed" },
-]
+  { file: "sounds/something.mp3", key: "Q" },
+  { file: "sounds/Basic_Rock_135.mp3", key: "W" },
+  { file: "sounds/Boom_Chuk_130.mp3", key: "E" },
+  { file: "sounds/Cymbal_Groove.mp3", key: "A" },
+  { file: "sounds/Tamba_112.mp3", key: "S" },
+  { file: "sounds/tense.mp3", key: "D" },
+  { file: "sounds/heavy-metal.mp3", key: "Z" },
+  { file: "sounds/Laugh+2.mp3", key: "X" },
+  { file: "sounds/Giggle+6.mp3", key: "C" },
+];
